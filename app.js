32d9c51882c84584c0169b46c18bbfd9b90e66f6
wzhscript <wzhscript@126.com>
2014-12-05 19:43:37 +0800

    fix bugs

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'test')));
 
 
 app.get('/', user.requireUser(), routes.index);
-app.post('/saveScene', routes.saveScene);
+app.post('/addScene', routes.addScene);
 app.post('/updateSceneByName', routes.updateSceneByName);
 app.get('/loadScene', routes.loadScene);
 app.get('/getAllScenes', routes.getAllScenes);
