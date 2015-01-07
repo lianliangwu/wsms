b6090575aba2fd2ba42cf1ef659fce31d259ad3d
will <will@will-ThinkPad-E420.(none)>
2015-01-07 11:55:53 +0800

    add dropzone plugin

--- app.js
+++ app.js
@@ -96,6 +96,29 @@ app.get('/getTags', rc.getTags);
 app.get('/getRHG', rc.getVersionHistory);
 
 app.get('/vedio', function (req, res) {res.render('vedio_index');});
+app.post('/upload', function(req, res) {
+	var tempfile    = req.files.filename.path;
+	var origname    = req.files.filename.name;
+	var writestream = gfs.createWriteStream({ filename: origname });
+	// open a stream to the temporary file created by Express...
+	fs.createReadStream(tempfile)
+		.on('end', function() {
+			res.send('upload OK');
+		})
+		.on('error', function() {
+			res.send('upload ERR');
+		})
+		// and pipe it to gfs
+		pipe(writestream);
+});
+app.get('/download', function(req, res) {
+	// TODO: set proper mime type + filename, handle errors, etc...
+	gfs
+	// create a read stream from gfs...
+	.createReadStream({ filename: req.param('filename') })
+	// and pipe it to Express' response
+	.pipe(res);
+});
 
 // synchronus operation
 io.on('connection', function(socket){
--- models/db.js
+++ models/db.js
@@ -1,13 +1,50 @@
 "use strict";
 // Bring Mongoose into the project
 var mongoose = require( 'mongoose' );
+// Bring gridfs-stream into the project
+var Grid = require('gridfs-stream');
+var fs = require('fs');
+var express = require('express');
+
+Grid.mongo = mongoose.mongo;
+
 // Build the connection string
 var dbURI = 'mongodb://localhost/wsms';
 // Create the database connection
-mongoose.connect(dbURI);
+var conn = mongoose.connect(dbURI);
+
+
 
 mongoose.connection.on('connected', function () {
 	console.log('Mongoose connected to ' + dbURI);
+
+	var gfs = Grid(conn, mongoose.mongo);
+	// var app = express();
+
+	// app.use(express.bodyParser());
+	// app.post('/upload', function(req, res) {
+	// 	var tempfile    = req.files.filename.path;
+	// 	var origname    = req.files.filename.name;
+	// 	var writestream = gfs.createWriteStream({ filename: origname });
+	// 	// open a stream to the temporary file created by Express...
+	// 	fs.createReadStream(tempfile)
+	// 		.on('end', function() {
+	// 			res.send('upload OK');
+	// 		})
+	// 		.on('error', function() {
+	// 			res.send('upload ERR');
+	// 		})
+	// 		// and pipe it to gfs
+	// 		pipe(writestream);
+	// });
+	// app.get('/download', function(req, res) {
+	// 	// TODO: set proper mime type + filename, handle errors, etc...
+	// 	gfs
+	// 	// create a read stream from gfs...
+	// 	.createReadStream({ filename: req.param('filename') })
+	// 	// and pipe it to Express' response
+	// 	.pipe(res);
+	// });
 });
 
 mongoose.connection.on('error',function (err) {
